3c0ef5f2972bda5ae4e1f171e0707da038198d29
valeriy <val6785@mail.ru>
2017-03-25 20:10:48 +0200

    add tests

--- tests/CollectionTest.php
+++ tests/CollectionTest.php
@@ -4,6 +4,9 @@ namespace Collection\Tests;
 
 use Collection\Collection;
 use ReflectionProperty;
+use ArrayIterator;
+use ReflectionClass;
+use ReflectionMethod;
 
 
 class CollectionTest extends \PHPUnit_Framework_TestCase {
@@ -12,12 +15,16 @@ class CollectionTest extends \PHPUnit_Framework_TestCase {
 
     protected $property;
 
+    protected $class;
+
     public function setUp() {
 
         $this->collection = new Collection();
 
         $this->property = new ReflectionProperty($this->collection, 'items');
         $this->property->setAccessible(true);
+
+        $this->class = new ReflectionClass($this->collection);
     }
 
     public function testInitialize() {
@@ -137,6 +144,186 @@ class CollectionTest extends \PHPUnit_Framework_TestCase {
 
         $this->assertEmpty($this->property->getValue($this->collection));
         $this->assertTrue($this->collection->isEmpty());
+    }
+
+    public function testCount() {
+
+        $count = $this->collection->set('foo', 'bar')->count();
+
+        $this->assertTrue($count === 1);
+    }
+
+    public function testGetIterator() {
+
+        $arrayIterator = $this->collection->set('foo', 'bar')->getIterator();
+
+        $this->assertInstanceOf(ArrayIterator::class, $arrayIterator);
+        $this->assertCount(1, $arrayIterator->getArrayCopy());
+    }
+
+    public function testClone() {
+
+        $clone = clone $this->collection->set('foo', 'bar');
+
+        $this->assertEquals($clone, $this->collection);
+        $this->assertNotSame($clone, $this->collection);
+    }
+
+    public function testDeepCopyArray() {
+
+        $method = $this->class->getMethod('deepCopyArray');
+        $method->setAccessible(true);
+
+        $array = [
+            'foo' => 'bar',
+            'php' => 7
+        ];
+
+        $copyArray = $method->invoke($this->collection, $array);
+
+        $this->assertEquals($array, $copyArray);
+        $this->assertSame($array, $copyArray);
+
+
+        array_push($array, [ 'bar?' => 'foo!' ]);
+
+        $this->assertNotEquals($array, $copyArray);
+    }
+
+    public function testEach() {
+
+        $this->collection->set('foo', 'bar');
+        $this->collection->set('php', [
+            6 => false,
+            7 => true
+        ]);
+
+        $array = [];
+
+        $this->collection->each(function($value) use(&$array) {
+            $array[] = $value;
+        });
+
+
+        $values = array_values($this->property->getValue($this->collection));
+
+        $this->assertSame($values, $array);
+    }
+
+    public function testPush() {
+
+        $this->collection->push('foo')->push('bar');
+
+        $items = $this->property->getValue($this->collection);
+
+        $this->assertSame($items[count($items) - 1], 'bar');
+    }
+
+    public function testPop() {
+
+        $lastItem = $this->collection->push('foo')->push('bar')->pop();
+
+        $items = $this->property->getValue($this->collection);
+
+        $this->assertSame($lastItem, 'bar');
+        $this->assertCount(1, $items);
+    }
+
+    public function testFront() {
+
+        $this->collection->push('foo')->front('bar');
+
+        $items = $this->property->getValue($this->collection);
+
+        $this->assertSame($items[0], 'bar');
+    }
+
+    public function testShift() {
+
+        $firstItem = $this->collection->push('foo')->push('bar')->shift();
+
+        $items = $this->property->getValue($this->collection);
+
+        $this->assertSame($firstItem, 'foo');
+        $this->assertCount(1, $items);
+    }
+
+    public function testFirst() {
+
+        $firstItem = $this->collection->push('foo')->push('bar')->first();
+
+        $items = $this->property->getValue($this->collection);
+
+        $this->assertSame($firstItem, 'foo');
+        $this->assertCount(2, $items);
+    }
+
+    public function testLast() {
+
+        $lastItem = $this->collection->push('foo')->push('bar')->last();
+
+        $items = $this->property->getValue($this->collection);
+
+        $this->assertSame($lastItem, 'bar');
+        $this->assertCount(2, $items);
+    }
+
+    public function testOffsetUnset() {
+
+        $this->collection->push('foo')->push('bar')->offsetUnset(0);
+
+        $items = $this->property->getValue($this->collection);
+
+        $this->assertCount(1, $items);
+    }
+
+    public function testRemove() {
+
+        $this->collection->set('foo', 'bar');
+        $this->collection->set('php', [
+            6 => false,
+            7 => true
+        ])->remove('foo');
+
+
+        $items = $this->property->getValue($this->collection);
+
+        $this->assertCount(1, $items);
+        $this->assertArrayHasKey('php', $items);
+        $this->assertArrayNotHasKey('foo', $items);
+    }
+
+    public function testToArray() {
+
+        $this->collection->set('foo', 'bar');
+        $this->collection->set('php', [
+            6 => false,
+            7 => true
+        ]);
+
+        $items = $this->property->getValue($this->collection);
+
+        $itemsAsArray = $this->collection->toArray();
+
+        $this->assertInternalType('array', $itemsAsArray);
+        $this->assertSame($items, $itemsAsArray);
+    }
+
+    public function testToJson() {
+
+        $this->collection->set('foo', 'bar');
+        $this->collection->set('php', [
+            6 => false,
+            7 => true
+        ]);
+
+        $items = json_encode($this->property->getValue($this->collection));
+
+        $itemsAsJson = $this->collection->toJson();
+
+        $this->assertInternalType('string', $itemsAsJson);
+        $this->assertSame($items, $itemsAsJson);
+        $this->assertJson($itemsAsJson);
     }
 
 
